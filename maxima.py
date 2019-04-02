2e019acccb9ce4d770778183c7aab7ed767d0ecc
Zoey Hearn <zoeyhearn@Zoeys-MacBook-Pro.local>
2019-04-02 15:28:54 +0200

    test commit 2

--- maxima.py
+++ maxima.py
@@ -14,9 +14,15 @@ def find_maxima(x):
     """
 
     idx = []
+    if x[0] > x[1]:
+        idx.append(0)
     for i in range(len(x)-1):
+        if i < 0:
+            continue
+
         # `i` is a local maximum if the signal decreases before and after it
         if x[i-1] < x[i] and x[i+1] < x[i]:
             idx.append(i)
-    return idx
-
+    if x[-1] > x[-2]:
+        idx.append(len(x)-1)
+    return list(set(idx))
--- test_maxima.py
+++ test_maxima.py
@@ -4,7 +4,7 @@ import pytest
 
 test_cases = [([0, 1, 2, 1, 2, 1, 0],[2,4]),
               ([-i**2 for i in range(-3, 4)], [3]),
-              ([np.sin(2*alpha) for alpha in np.linspace(0.0, 5.0, 100)], [16,78]),
+              ([np.sin(2*alpha) for alpha in np.linspace(0.0, 5.0, 100)], [16, 78]),
               ([4, 2, 1, 3, 1, 2],[0,3,5]),
               ([4, 2, 1, 3, 1, 5], [0,3,5]),
               ([4, 2, 1, 3, 1], [0,3])]
